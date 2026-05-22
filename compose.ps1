2172e91fd97249f3cf854aab875697f206ee229a
LoïcVasile <lvasile@progisem.com>
2026-05-22 11:52:46 +0200

    ADD script to run compose on different environments

---
+++ compose.ps1
@@ -0,0 +1,26 @@
+param($env = "dev")
+
+# Lance le docker compose en fonction de l'environnement spécifié ou en dev si non spécifié
+function Start-Compose
+{
+    param($env)
+
+    if($env -ne "dev" -and $env -ne "prod")
+    {
+        throw "Invalid environment specified: `"$env`".`nPlease use `"dev`" or `"prod`"."
+    }
+    
+    if($env -eq "dev")
+    {
+        echo "Starting compose for dev environment"
+        docker compose -f ./compose.dev.yaml up -d --build
+    }
+
+    if($env -eq "prod")
+    {
+        echo "Starting compose for prod environment"
+        docker compose -f ./compose.prod.yaml up -d --build
+    }
+}
+
+Start-Compose -env $env
